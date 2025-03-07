b953a75520eed915ae68b2197a2f4faac2c214cb
Konstantin Shilkov <konst.shilkov@gmail.com>
2025-03-07 16:53:12 +0100

    feat: filter categories to exclude supercategories in update and create pages

--- src/app/dashboard/categories/[id]/update/page.js
+++ src/app/dashboard/categories/[id]/update/page.js
@@ -1,26 +1,20 @@
 "use server";
 import EntityUpdate from "@/_components/dashboard/cqp/EntityUpdate";
 import getAccessToken from "@/_lib/getAccessTokenUtil";
-import {
-  getAllAsList,
-  getOneById,
-  getAllAsSimpleList,
-} from "@/_lib/entityApiUtil";
+import { getOneById, getAllAsSimpleList } from "@/_lib/entityApiUtil";
 
 const page = async (props) => {
   const { accessToken, session } = getAccessToken();
   const params = await props.params;
   const category = await getOneById("categories", params.id, accessToken);
   const categories = await getAllAsSimpleList("categories", accessToken);
-  const questions = await getAllAsList("questions", accessToken);
 
   return (
     <EntityUpdate
       session={session}
       entity={category}
       entityName="categories"
-      categories={categories}
-      questions={questions}
+      categories={categories.filter((c) => c.supercategory == null)}
     />
   );
 };
--- src/app/dashboard/categories/create/page.js
+++ src/app/dashboard/categories/create/page.js
@@ -1,17 +1,17 @@
 import getAccessToken from "@/_lib/getAccessTokenUtil";
-import { getAllAsList, getAllAsSimpleList } from "@/_lib/entityApiUtil";
+import { getAllAsSimpleList } from "@/_lib/entityApiUtil";
 import EntityUpdate from "@/_components/dashboard/cqp/EntityUpdate";
 
 const page = async () => {
   const { accessToken, session } = getAccessToken();
   const categories = await getAllAsSimpleList("categories", accessToken);
-  
+
   return (
     <EntityUpdate
       session={session}
       entity={null}
       entityName="categories"
-      categories={categories}
+      categories={categories.filter((c) => c.supercategory == null)}
     />
   );
 };
