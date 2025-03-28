85b22309e73754a64f74ef760212a5c2e9d4cd36
Konstantin Shilkov <konst.shilkov@gmail.com>
2025-03-28 17:19:21 +0100

    feat: add textToSpeechStep function for improved phrase playback control and step management

--- src/_lib/textToSpeech.js
+++ src/_lib/textToSpeech.js
@@ -4,3 +4,12 @@ export const textToSpeech = (text) => {
   talk.lang = "fr-FR";
   speechSynthesis.speak(talk);
 };
+
+export const textToSpeechStep = (phraseSplit, stepPlayId, setStepPlayId) => {
+  // let phraseSplit = phrase.split(" ").filter((e) => e != "");
+  textToSpeech(phraseSplit[stepPlayId == -1 ? 0 : stepPlayId]);
+  setStepPlayId((prev) => ++prev);
+  if (stepPlayId >= phraseSplit.length - 1) {
+    setStepPlayId(0);
+  }
+};
